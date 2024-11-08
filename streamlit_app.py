e26bd75a6d70e8909eda78fd89cc21e660fe3bd4
koungin <20242514@sdhs.gwe.hs.kr>
2024-11-08 09:19:09 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,15 @@ import streamlit as st
 import pandas as pd
 
 # 타이틀 설정
-st.title("Streamlit 기본 예제")
+st.title("나의 첫번째 서비스")
 
 # 서브타이틀
 st.subheader("기본적인 표와 HTML 메시지 생성")
 
 # 표 데이터 생성
 data = {
-    '이름': ['홍길동', '김철수', '이영희'],
-    '나이': [25, 30, 22],
+    '이름': ['김종인', '도경수', '변백현'],
+    '나이': [30, 31, 31],
     '직업': ['학생', '회사원', '디자이너']
 }
 df = pd.DataFrame(data)
